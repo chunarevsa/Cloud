21488dba4c70d6f89f845cd40e10c01d7e2116b5
Chunarev <chunarev.se.a@gmail.com>
2022-01-23 18:33:52 +0500

    1

--- src/main/java/com/chunarevsa/cloud/service/ScheduledTasks.java
+++ src/main/java/com/chunarevsa/cloud/service/ScheduledTasks.java
@@ -1,8 +1,21 @@
 package com.chunarevsa.cloud.service;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,25 +31,24 @@ public class ScheduledTasks {
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
 	@Scheduled(fixedRate = 300000)
-	public void reportCurrentTime() {
+	public void updateInfo() throws ParserConfigurationException, MalformedURLException, IOException, SAXException {
+    
+    log.info("Старт обновления базы данных", dateFormat.format(new Date()));
 
-		String url = "http://api.nbp.pl/api/exchangerates/rates/a/gbp/2012-01-01/2012-01-31/";   
-    try
-    {
-        DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
-        DocumentBuilder b = f.newDocumentBuilder();
-        Document doc = b.parse(url);
 
-        doc.getDocumentElement().normalize();
-        System.out.println ("Root element: " + 
-                    doc.getDocumentElement().getNodeName());
+	String url = "https://s3.amazonaws.com/cloudaware-test";
+    DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
+    f.setNamespaceAware(false);
+    f.setValidating(false);
+    DocumentBuilder b = f.newDocumentBuilder();
+    URLConnection urlConnection = new URL(url).openConnection();
+    urlConnection.addRequestProperty("Accept", "application/xml");
+    Document doc = b.parse(urlConnection.getInputStream());
+    doc.getDocumentElement().normalize();
+    System.out.println ("Root element: " +  doc.getDocumentElement().getNodeName());
 
-    }
-    catch (Exception e)
-    {
-        e.printStackTrace();
-    }
-		log.info("База данных успешно обновлена", dateFormat.format(new Date()));
+
+	log.info("База данных успешно обновлена", dateFormat.format(new Date()));
 	}
 
 }
