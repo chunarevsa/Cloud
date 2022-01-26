22f90eeaee5432d08c2da9e3c67b75f127ac7e93
Chunarev_Sergey <chunarev.se.a@gmail.com>
2022-01-26 11:15:31 +0500

    1

--- src/main/java/com/chunarevsa/cloud/service/ScheduledTasks.java
+++ src/main/java/com/chunarevsa/cloud/service/ScheduledTasks.java
@@ -1,14 +1,21 @@
 package com.chunarevsa.cloud.service;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
@@ -21,6 +28,7 @@ import com.chunarevsa.cloud.entity.TempContent;
 import com.chunarevsa.cloud.repository.ContentsRepository;
 import com.chunarevsa.cloud.repository.OwnerRepository;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,8 +61,10 @@ public class ScheduledTasks {
 
 
 	String url = "https://s3.amazonaws.com/cloudaware-test";
+
     
-    SAXParserFactory factory = SAXParserFactory.newInstance(); 
+    // Работа на прямую через ссылку
+    /* SAXParserFactory factory = SAXParserFactory.newInstance(); 
     factory.setNamespaceAware(false);
     factory.setValidating(false);
 
@@ -63,12 +73,59 @@ public class ScheduledTasks {
 
     SAXParser parser = factory.newSAXParser(); 
     AdvancedXMLHandler handler = new AdvancedXMLHandler();
-    parser.parse(urlConnection.getInputStream(), handler);
+    parser.parse(urlConnection.getInputStream(), handler);  */
 
-    System.err.println("Успешный парсинг");
+    // Для парсинга с фаила
+    /* SAXParserFactory factory = SAXParserFactory.newInstance();
+    SAXParser parser = factory.newSAXParser();
+
+    AdvancedXMLHandler handler = new AdvancedXMLHandler();
+    parser.parse(new File("src/main/resources/2cloudaware-test.xml"), handler); */
+
+    // Скачивает и парсит
+    /* System.err.println("1");
+    System.err.println("2");
+    URL website = new URL(url);
+    System.err.println("3");
+    ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+    System.err.println("4");
+    FileOutputStream fos = new FileOutputStream("src/main/resources/temp/cloudAw.xml");
+    System.err.println("5");
+    fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+    System.err.println("6"); */
+    URL website = new URL(url);
+    
+    //Files.deleteIfExists(Paths.get(path));
+
+    File file = new File("");
+
+    //FileUtils.copyURLToFile(website, file);
+    
+    System.err.println("TYT");
+    InputStream inputStream = website.openStream();
+    Files.copy(inputStream, file.toPath());
+
+
+    SAXParserFactory factory = SAXParserFactory.newInstance();
+    SAXParser parser = factory.newSAXParser();
+
+    AdvancedXMLHandler handler = new AdvancedXMLHandler();
+    parser.parse(file, handler);
+    
+    
+
+    log.info("Успешный парсинг");
+
     int newObject = 0; //новых записей
     int editedObject = 0; // измененных записей
     int oldObject = 0;
+
+    /* ArrayList<TempContent> tempCont = new ArrayList<>();
+    for (int i = 0; i < 50; i++) {
+        tempCont.add(tempContents.get(i));
+    } */
+
+
     for (TempContent tempContent: tempContents) {
 
         Content newContent = contentsRepository.findByContentKey(tempContent.getContentKey()).orElse(null);
@@ -108,31 +165,9 @@ public class ScheduledTasks {
         } else {
             oldObject++;
         }
-
-        
-        
-
-        //System.err.println(savedOwner);
-
-        // первый вар: сохранять всех contents как в StoreApi
-        /* System.err.println("первый вар");
-
-        owner.getContents().add(newContent);
-        saveContents(owner.getContents()); */
-
-        // второй вар : сохранять только новый контент
-        /*System.err.println("второй вар");
-        owner.getContents().add(newContent);
-        saveContent(newContent); */
-
-        // третий вар: добавление уже сохраненного content
-        /* System.err.println("3 вар");
-        Content savedContent = saveContent(newContent);
-        owner.getContents().add(savedContent); */
         
     } 
     
-    System.err.println("Количесвто :" + tempContents.size());
     ownerRepository.findAll().forEach(owner -> System.out.println(owner.getDisplayName()));
     ownerRepository.findAll().forEach(owner -> System.out.println(owner.getBucketId()));
     tempContents.clear();
@@ -140,14 +175,18 @@ public class ScheduledTasks {
     System.err.println("Количество измененных записей :" + editedObject);
     System.err.println("Количество объектов без изменений :" + oldObject);
     
- 
+    //fos.close();
 	log.info("База данных успешно обновлена :" + dateFormat.format(new Date()));
 	}
 
     private void validateAndSaveOwner(TempContent tempContent, Content newContent) {
         
+        String tempId = "14fbada9d6aac53a2d851e6c777ffea7cd9ac4d213bee68af9f5d9b247c20c04";
+        if (!tempContent.getBucketId().equalsIgnoreCase(tempId)) {
+            System.err.println(tempContent.getContentKey());
+        }
         Owner owner = ownerRepository.findByBucketId(tempContent.getBucketId()).orElse(null);
-
+        
         if (owner == null) {
             System.err.println("Новый владелец");
             owner = new Owner();
@@ -155,24 +194,25 @@ public class ScheduledTasks {
             owner.setDisplayName(tempContent.getDisplayName());
         } 
 
-        // четвёртый вар. новый список через set
         owner.getContents().add(newContent);
         Set<Content> contents = owner.getContents();
         owner.setContents(contents); 
 
         ownerRepository.save(owner);
+
     }
 
     private static class AdvancedXMLHandler extends DefaultHandler {
 
-        private String key;
+        private String contentKey;
         private String lastModified;
         private String eTag;
         private String size;
-        private String lastElementName;
         private String bucketId;
         private String displayName;
         private String storageClass;
+
+        private String lastElementName;
 
 
         @Override
@@ -197,7 +237,7 @@ public class ScheduledTasks {
         public void endElement(String uri, String localName, String qName) throws SAXException {
             //System.err.println("endElement");
 
-            if ( (key != null && !key.isEmpty()) && 
+            if ( (contentKey != null && !contentKey.isEmpty()) && 
                  (lastModified != null && !lastModified.isEmpty()) &&
                  (eTag != null && !eTag.isEmpty()) &&
                  (size != null && !size.isEmpty()) &&
@@ -205,9 +245,9 @@ public class ScheduledTasks {
                  (displayName != null && !displayName.isEmpty()) &&
                  (storageClass != null && !storageClass.isEmpty()) ) {
                 
-                tempContents.add(new TempContent(key, lastModified, eTag, size, bucketId, displayName, storageClass));
+                tempContents.add(new TempContent(contentKey, lastModified, eTag, size, bucketId, displayName, storageClass));
 
-                key = null;
+                contentKey = null;
                 lastModified = null;
                 eTag = null;
                 size = null;
@@ -228,24 +268,33 @@ public class ScheduledTasks {
 
         @Override
         public void characters(char[] ch, int start, int length) throws SAXException {
-            //System.err.println("characters");
 
+            String tempId = "14fbada9d6aac53a2d851e6c777ffea7cd9ac4d213bee68af9f5d9b247c20c04";
             String info = new String(ch, start, length);
-            
+
             String information = info.replace("\"", "").replace("\n", "").trim();
-            //System.err.println(information);
 
             if (!information.isEmpty()) {
                 if (lastElementName.equalsIgnoreCase("Key"))
-                    key = information;
+                    contentKey = information;
                 if (lastElementName.equalsIgnoreCase("lastModified"))
                     lastModified = information;
                 if (lastElementName.equalsIgnoreCase("eTag"))
                     eTag = information;
                 if (lastElementName.equalsIgnoreCase("size"))
                     size = information;
-                if (lastElementName.equals("ID"))
+                if (lastElementName.equals("ID")) {
+                    if (!information.equalsIgnoreCase(tempId)) {
+                        System.err.println("information :" + information);
+                        System.err.println("char[] : " + ch.toString());
+                        System.err.println("start : " + start);
+                        System.err.println("length : " + length);
+                        System.err.println();
+                        
+                    }
                     bucketId = information; 
+                    }
+                    
                 if (lastElementName.equalsIgnoreCase("displayName"))
                     displayName = information; 
                 if (lastElementName.equalsIgnoreCase("storageClass"))
